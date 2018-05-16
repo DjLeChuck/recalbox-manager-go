88bf9194af069e0e7631e26b17934037dd0cc368
DjLeChuck <djlechuck@gmail.com>
2018-05-16 23:02:21 +0200

    Add i18n
    - Add middlware
    - Add 3 locale files for testing
    - Test using "each" in template but it does not work for the moment

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/kataras/iris"
 
+	"github.com/kataras/iris/middleware/i18n"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
 )
@@ -14,17 +15,36 @@ func main() {
 	app.Use(recover.New()) // recover from any http-relative panics
 	app.Use(logger.New())  // log the requests to the terminal.
 
+	availableLanguages := map[string]string{
+		"en-US": "./locales/en.ini",
+		"fr-FR": "./locales/fr.ini",
+		"zh-CN": "./locales/zh.ini",
+	}
+
+	globalLocale := i18n.New(i18n.Config{
+		Default:      "zh-CN",
+		URLParameter: "lang",
+		Languages:    availableLanguages})
+	app.Use(globalLocale)
+
 	app.StaticWeb("/css", "./assets/css")
 	app.StaticWeb("/js", "./assets/js")
 
-	tmpl := iris.Pug("./templates", ".pug").Layout("layouts/default.pug")
+	tmpl := iris.Pug("./templates", ".pug")
 	tmpl.Reload(true) // reload templates on each request (development mode)
 
 	app.RegisterView(tmpl)
 
+	app.Use(func(ctx iris.Context) {
+		ctx.ViewLayout("layouts/default.pug")
+		ctx.ViewData("RecalboxManagerTitle", ctx.Translate("Recalbox Manager"))
+		ctx.ViewData("CurrentLang", ctx.Values().GetString(ctx.Application().ConfigurationReadOnly().GetTranslateLanguageContextKey()))
+		ctx.ViewData("AvailableLang", availableLanguages)
+
+		ctx.Next()
+	})
+
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.ViewData("Title", "Hi Page")
-		ctx.ViewData("Name", "iris")
 		ctx.View("views/home.pug")
 	})
 
