ecdceb6a925cf2c8007407751b0f074c3613b9c4
DjLeChuck <djlechuck@gmail.com>
2018-05-19 10:48:28 +0200

    Update error handling
    - Add a specific message for 404 Not Found

--- main.go
+++ main.go
@@ -77,6 +77,13 @@ func main() {
 	app.Favicon("./assets/favicon.png")
 
 	app.OnAnyErrorCode(func(ctx iris.Context) {
+		message := "Please, go to #[strong the Support page] and execute #[strong recalbox-support.sh]"
+
+		if ctx.GetStatusCode() == 404 {
+			message = "The page you requested was not found."
+		}
+
+		ctx.ViewData("Message", message)
 		ctx.View("views/error.pug")
 	})
 
