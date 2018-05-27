7d81fb8873fea7033b8f68d5af299008eef4263b
DjLeChuck <djlechuck@gmail.com>
2018-05-27 23:01:37 +0200

    Update confirmModal module
    - A callback can be used when the yes button is clicked

--- assets/js/modules/confirmModal.js
+++ assets/js/modules/confirmModal.js
@@ -3,10 +3,23 @@ $(function () {
     var $link = $(event.relatedTarget);
     var $modal = $(this);
     var content = $link.data("message");
+    var callback = $link.data("callback");
+    var $yesBtn = $modal.find("[data-href]");
 
-    $modal.find("[data-href]").attr("href", $link.attr("href"));
+    $yesBtn.attr("href", $link.attr("href"));
+    $yesBtn.off("click");
     $modal.find(".modal-body").html(
       content.replace("%s", "<strong>" + $link.data("message-arg") + "</strong>")
     );
+
+    if ("" !== callback) {
+      $yesBtn.on("click", function () {
+        $modal.modal("hide");
+
+        window[callback](event);
+
+        return false;
+      });
+    }
   });
 });
