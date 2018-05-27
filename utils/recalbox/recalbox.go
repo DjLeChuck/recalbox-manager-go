49c90750cec4dd3e31dccf5a6b26dce11cb52ea1
DjLeChuck <djlechuck@gmail.com>
2018-05-27 15:41:22 +0200

    Add util to generate a pseudo UUID

--- utils/recalbox/recalbox.go
+++ utils/recalbox/recalbox.go
@@ -1,6 +1,7 @@
 package recalbox
 
 import (
+	"crypto/rand"
 	"fmt"
 	"os/exec"
 	"reflect"
@@ -73,4 +74,15 @@ func ProcessRecalboxSettingsForm(data map[string]interface{}) (err error) {
 	}
 
 	return nil
+}
+
+// PseudoUuid generates a sort of UUID.
+func PseudoUuid() (string, error) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
 }
